a92918b5c39f33be8e2d16e4f9e225800068fd32
Diego12132025 <DiegoWork2004@gmail.com>
2025-05-20 16:37:33 +0100

    Refactor AutorService to improve structure and readability

--- libros_y_autores/src/main/java/com/actividad2/libros_y_autores/service/AutorService.java
+++ libros_y_autores/src/main/java/com/actividad2/libros_y_autores/service/AutorService.java
@@ -1,9 +1,22 @@
 package com.actividad2.libros_y_autores.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.actividad2.libros_y_autores.model.Autor;
+import com.actividad2.libros_y_autores.repository.AutorRepository;
 
 @Service
 public class AutorService {
-    // Aquí puedes definir los métodos que necesites para interactuar con los autores
-    // Por ejemplo, un método para obtener todos los autores, otro para guardar un nuevo autor, etc.
+
+    private AutorRepository autorRepository;
+
+    public AutorService(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
+
+    public List<Autor> listAll() { return autorRepository.findAll(); }
+    /* Añadir método ver autor con sus libros ... */
+    public Autor createAutor(Autor autor) { return autorRepository.save(autor); }
 }
