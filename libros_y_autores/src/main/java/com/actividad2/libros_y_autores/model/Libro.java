3401b2030384285e3b60c3d314582679fd21bfc3
IzanKing2 <izanwork2@gmail.com>
2025-05-20 20:34:09 +0100

    Remove unique constraint from ISBN field in Libro class

--- libros_y_autores/src/main/java/com/actividad2/libros_y_autores/model/Libro.java
+++ libros_y_autores/src/main/java/com/actividad2/libros_y_autores/model/Libro.java
@@ -1,7 +1,6 @@
 package com.actividad2.libros_y_autores.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,7 +19,6 @@ public class Libro {
 
     private String titulo;
 
-    @Column(unique = true)
     private String isbn;
 
     private int anioPublicacion;
